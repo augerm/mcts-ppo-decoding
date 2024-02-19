6e6f89de0ca276075d691a7c751b7e7d9b3fcd86
augerm <michaelauger23@gmail.com>
2024-02-19 09:08:17 -0600

    Fork MCTS implementation

--- tree_search_decoding/mcts.py
+++ tree_search_decoding/mcts.py
@@ -1,60 +1,120 @@
+''' Implementation forked from https://github.com/pbsinclair42/MCTS/tree/master '''
+from __future__ import division
+
+import time
 import math
 import random
 
-class MCTSNode:
-    def __init__(self, game_state, parent=None):
-        self.game_state = game_state
+
+def randomPolicy(state):
+    while not state.isTerminal():
+        try:
+            action = random.choice(state.getPossibleActions())
+        except IndexError:
+            raise Exception("Non-terminal state has no possible actions: " + str(state))
+        state = state.takeAction(action)
+    return state.getReward()
+
+
+class treeNode():
+    def __init__(self, state, parent):
+        self.state = state
+        self.isTerminal = state.isTerminal()
+        self.isFullyExpanded = self.isTerminal
         self.parent = parent
-        self.children = []
-        self.wins = 0
-        self.visits = 0
-        self.untried_actions = self.game_state.get_legal_actions()
+        self.numVisits = 0
+        self.totalReward = 0
+        self.children = {}
 
-    def ucb1_select_child(self):
-        """Select a child node using UCB1 policy."""
-        C = 1.41  # Exploration parameter
-        chosen_child = max(self.children, key=lambda x: x.wins / x.visits + C * math.sqrt(2 * math.log(self.visits) / x.visits))
-        return chosen_child
+    def __str__(self):
+        s=[]
+        s.append("totalReward: %s"%(self.totalReward))
+        s.append("numVisits: %d"%(self.numVisits))
+        s.append("isTerminal: %s"%(self.isTerminal))
+        s.append("possibleActions: %s"%(self.children.keys()))
+        return "%s: {%s}"%(self.__class__.__name__, ', '.join(s))
 
-    def add_child(self, action, game_state):
-        """Add a new child node for the given action."""
-        child_node = MCTSNode(game_state=game_state, parent=self)
-        self.untried_actions.remove(action)
-        self.children.append(child_node)
-        return child_node
+class mcts():
+    def __init__(self, timeLimit=None, iterationLimit=None, explorationConstant=1 / math.sqrt(2),
+                 rolloutPolicy=randomPolicy):
+        if timeLimit != None:
+            if iterationLimit != None:
+                raise ValueError("Cannot have both a time limit and an iteration limit")
+            # time taken for each MCTS search in milliseconds
+            self.timeLimit = timeLimit
+            self.limitType = 'time'
+        else:
+            if iterationLimit == None:
+                raise ValueError("Must have either a time limit or an iteration limit")
+            # number of iterations of the search
+            if iterationLimit < 1:
+                raise ValueError("Iteration limit must be greater than one")
+            self.searchLimit = iterationLimit
+            self.limitType = 'iterations'
+        self.explorationConstant = explorationConstant
+        self.rollout = rolloutPolicy
 
-    def update(self, result):
-        """Update this node's data from the child node."""
-        self.visits += 1
-        self.wins += result
+    def search(self, initialState, needDetails=False):
+        self.root = treeNode(initialState, None)
 
-def mcts(root_state, iterations):
-    root_node = MCTSNode(game_state=root_state)
+        if self.limitType == 'time':
+            timeLimit = time.time() + self.timeLimit / 1000
+            while time.time() < timeLimit:
+                self.executeRound()
+        else:
+            for i in range(self.searchLimit):
+                self.executeRound()
 
-    for _ in range(iterations):
-        node = root_node
-        state = root_state.clone()
+        bestChild = self.getBestChild(self.root, 0)
+        action=(action for action, node in self.root.children.items() if node is bestChild).__next__()
+        if needDetails:
+            return {"action": action, "expectedReward": bestChild.totalReward / bestChild.numVisits}
+        else:
+            return action
 
-        # Selection
-        while node.untried_actions == [] and node.children != []:  # node is fully expanded and non-terminal
-            node = node.ucb1_select_child()
-            state.do_action(node.game_state)
+    def executeRound(self):
+        """
+            execute a selection-expansion-simulation-backpropagation round
+        """
+        node = self.selectNode(self.root)
+        reward = self.rollout(node.state)
+        self.backpropogate(node, reward)
 
-        # Expansion
-        if node.untried_actions != []:
-            action = random.choice(node.untried_actions)
-            state.do_action(action)
-            node = node.add_child(action, state)  # add child and descend tree
+    def selectNode(self, node):
+        while not node.isTerminal:
+            if node.isFullyExpanded:
+                node = self.getBestChild(node, self.explorationConstant)
+            else:
+                return self.expand(node)
+        return node
 
-        # Simulation
-        while state.get_legal_actions() != []:  # while state is non-terminal
-            # TODO: Explore instead of taking the first 
-            action = state.policy_model.predict(state)[0]
-            state.do_action(action)
+    def expand(self, node):
+        actions = node.state.getPossibleActions()
+        for action in actions:
+            if action not in node.children:
+                newNode = treeNode(node.state.takeAction(action), node)
+                node.children[action] = newNode
+                if len(actions) == len(node.children):
+                    node.isFullyExpanded = True
+                return newNode
 
-        # Backpropagation
+        raise Exception("Should never reach here")
+
+    def backpropogate(self, node, reward):
         while node is not None:
-            node.update(state.get_result())  # backpropagate from the expanded node and work back to the root node
+            node.numVisits += 1
+            node.totalReward += reward
             node = node.parent
 
-    return max(root_node.children, key=lambda c: c.visits).game_state  # return the move that was most visited
+    def getBestChild(self, node, explorationValue):
+        bestValue = float("-inf")
+        bestNodes = []
+        for child in node.children.values():
+            nodeValue = node.state.getCurrentPlayer() * child.totalReward / child.numVisits + explorationValue * math.sqrt(
+                2 * math.log(node.numVisits) / child.numVisits)
+            if nodeValue > bestValue:
+                bestValue = nodeValue
+                bestNodes = [child]
+            elif nodeValue == bestValue:
+                bestNodes.append(child)
+        return random.choice(bestNodes)
---
+++ tree_search_decoding/mcts_test.py
@@ -0,0 +1,74 @@
+import unittest
+from unittest.mock import MagicMock
+from mcts import treeNode, mcts
+
+class MockGameState:
+    def __init__(self, terminal=False, possible_actions=None, reward=0):
+        self.terminal = terminal
+        self.possible_actions = possible_actions or []
+        self.reward = reward
+
+    def isTerminal(self):
+        return self.terminal
+
+    def getPossibleActions(self):
+        return self.possible_actions
+
+    def takeAction(self, action):
+        # Simply return a new state with decremented actions for simplicity
+        return MockGameState(terminal=len(self.possible_actions) == 1, possible_actions=self.possible_actions[:-1], reward=self.reward)
+
+    def getReward(self):
+        return self.reward
+
+    def getCurrentPlayer(self):
+        # Mock player for simplicity, 1 for player 1's turn, -1 for player 2's turn
+        return 1
+
+class TestMCTS(unittest.TestCase):
+    def test_treeNode_initialization(self):
+        state = MockGameState()
+        node = treeNode(state, None)
+        self.assertEqual(node.state, state)
+        self.assertFalse(node.isTerminal)
+        self.assertFalse(node.isFullyExpanded)
+        self.assertIsNone(node.parent)
+        self.assertEqual(node.numVisits, 0)
+        self.assertEqual(node.totalReward, 0)
+        self.assertEqual(len(node.children), 0)
+
+    def test_mcts_initialization(self):
+        mcts_instance = mcts(timeLimit=1000)
+        self.assertEqual(mcts_instance.timeLimit, 1000)
+        self.assertEqual(mcts_instance.limitType, 'time')
+        with self.assertRaises(ValueError):
+            mcts(timeLimit=1000, iterationLimit=100)
+
+    def test_selectNode_notFullyExpanded(self):
+        state = MockGameState(possible_actions=['a', 'b'])
+        mcts_instance = mcts(iterationLimit=100)  # Add an iterationLimit
+        node = treeNode(state, None)
+        selected_node = mcts_instance.selectNode(node)
+        self.assertIn(selected_node, node.children.values())
+
+    def test_expand(self):
+        state = MockGameState(possible_actions=['a', 'b'])
+        mcts_instance = mcts(iterationLimit=100)  # Add an iterationLimit
+        node = treeNode(state, None)
+        mcts_instance.expand(node)
+        self.assertIn('a', node.children.keys() or 'b' in node.children.keys())
+        self.assertFalse(node.isFullyExpanded)
+
+    def test_backpropogate(self):
+        state = MockGameState(possible_actions=['a'], reward=1)
+        root = treeNode(state, None)
+        child = treeNode(state.takeAction('a'), root)
+        mcts_instance = mcts(iterationLimit=100)  # Add an iterationLimit
+        mcts_instance.backpropogate(child, 1)
+        self.assertEqual(root.numVisits, 1)
+        self.assertEqual(root.totalReward, 1)
+        self.assertEqual(child.numVisits, 1)
+        self.assertEqual(child.totalReward, 1)
+
+if __name__ == '__main__':
+    unittest.main()
